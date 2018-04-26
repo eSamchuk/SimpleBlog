4a26315038ae0a3030483ee8d777f55469809797
eSamchuk <kilotonns@gmail.com>
2018-04-26 21:06:51 +0300

    added bundles of styles and scripts

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SimpleBlog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //бандл стилів для адміна
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
+
+            //бандл стилів для юзерів
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/site.css"));
+
+            //бандл скриптів для адміна
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-3.3.1.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+            //бандл скриптів для юзерів
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-3.3.1.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+
+        }
+    }
+}
